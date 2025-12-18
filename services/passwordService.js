0f5bfe31ae0aca4f64bb80fa9a34986ad9dd105b
WILLIAM MIRANDA AMORIM <willianmiranda127@gmail.com>
2025-12-18 19:21:58 -0400

    auth em desenvolvimento.

--- services/passwordService.js
+++ services/passwordService.js
@@ -9,15 +9,6 @@ export async function comparePassword(password, hashResult) {
   return await bcrypt.compare(password, hashResult);
 }
 
-// primeiro vamos importar as bibliotecas
-
-// receber a senha e transformar elas hash quando um usuario se cadastrar
-
-// quando for fazer login vou pegar essa senha e comparar ela com hash que foi salvo no banco sem acessar o banco,
-// usando o bcrypt se der True (verdadeiro)
-
-// se der true vai gerar um token
-
 // Serviço responsável por lidar com senhas
 // - transformar senha em hash no cadastro
 // - comparar senha em texto com hash salvo
--- services/tokenService.js
+++ services/tokenService.js
@@ -1,3 +1,6 @@
 import jwt from "jsonwebtoken";
 
-
+export function gerarToken(payload) {
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+  return token 
+}
